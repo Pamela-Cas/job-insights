47af3dd5538743da7faeee18f1d372132dfb5829
Pamela_Pereira <93348050+PamelaCP@users.noreply.github.com>
2023-03-02 14:37:13 -0300

    acertando get-unique-industries

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -1,9 +1,13 @@
 from typing import List, Dict
+from src.insights.jobs import read
 
 
 def get_unique_industries(path: str) -> List[str]:
-
-    raise NotImplementedError
+    industries = set()
+    for job in read(path):
+        if job["industry"] != "":
+            industries.add(job["industry"])
+        return industries
 
 
 def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
