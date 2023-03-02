595c455d4dc4efaca0127ce000fe1065b753d213
Pamela_Pereira <93348050+PamelaCP@users.noreply.github.com>
2023-03-02 14:21:32 -0300

    verificando erro!

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -1,13 +1,9 @@
-from jobs import read
 from typing import List, Dict
 
 
 def get_unique_industries(path: str) -> List[str]:
-    industries = set()
-    for job in read(path):
-        if job["industry"] != "":
-            industries.add(job["industry"])
-    return industries
+
+    raise NotImplementedError
 
 
 def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
