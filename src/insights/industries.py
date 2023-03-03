ff2f08fc6e68a8167f14d2832ebd432a2affec15
Pamela_Pereira <93348050+PamelaCP@users.noreply.github.com>
2023-03-03 02:12:15 -0300

    ajuste de lint em industries.py

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -10,7 +10,7 @@ def get_unique_industries(path: str) -> List[str]:
     return industries
 
 
-def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:  
+def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
     industries_filter = []
 
     for item in jobs:
